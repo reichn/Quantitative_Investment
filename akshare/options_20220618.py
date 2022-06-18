e478f6077ebd76dc69595ee1d410dcc098c8a32b
rechain22 <guorch29@gmail.com>
2022-06-18 13:51:25 +0800

    深交所也是动态加载

--- akshare/options_20220618.py
+++ akshare/options_20220618.py
@@ -2,12 +2,18 @@ import requests
 import datetime
 
 url = "http://www.sse.com.cn/assortment/options/date/"
+url2 = 'https://www.szse.cn/option/quotation/statistical/tabulate/'
 
 req = requests.get(url)
 req.encoding = "utf-8"
+req2 = requests.get(url2)
+req2.encoding = "utf-8"
 
 day = datetime.date.today()
 filename = day.strftime("%Y%m%d")
 
-with open("option" + filename + ".html", 'wb') as f:
+with open("option1_" + filename + ".html", 'wb') as f:
     f.write(req.content)
+with open("option2_" + filename + ".html", 'wb') as f:
+    f.write(req2.content)
+
