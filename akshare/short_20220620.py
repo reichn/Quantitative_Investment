a969640a31ae529afb344e6cc32e413c7784930b
rechain22 <guorch29@gmail.com>
2022-06-21 22:39:35 +0800

    融券添加默认值

--- akshare/short_20220620.py
+++ akshare/short_20220620.py
@@ -1,5 +1,7 @@
 from daily_data_20220617 import short
+import datetime
 
 if __name__ == "__main__":
-    d = input("enter the day: ")
+    t = datetime.date.today().strftime("%Y%m%d")
+    d = input(f"enter the day (today: {t}): ") or t
     print(short(d))
