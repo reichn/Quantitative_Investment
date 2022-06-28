ae1b38f49ca318d6ec93e9354f8b65c5d1e86983
rechain22 <guorch29@gmail.com>
2022-06-28 20:59:20 +0800

    融券 short() 没有数据的异常处理和信息输出

--- akshare/short_20220620.py
+++ akshare/short_20220620.py
@@ -1,3 +1,4 @@
+from typing import Type
 from daily_data_20220617 import short
 import datetime
 import time
@@ -9,15 +10,25 @@ if __name__ == "__main__":
     y = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y%m%d")
     ti = time.strftime("%H:%M")
 
-    d = input(f"enter the day (today: {t}=1, yestoday: {y}=2): ") or y
+    d = input(f"enter the day (today: {t}=1, yestoday: {y}=2): ")
     if d == "1":
         d = t
     elif d == "2":
         d = y
+    else:
+        d = t  # 默认值
 
     path = r"D:\GRC\我的坚果云\data_xlsx 数据记录\20220617 Daily data"
     with open(path + "\\" + d + "_short.txt", "a", encoding="utf-8") as f:
         f.write(ti + "\n")
-        f.write("融券、总市值、上市股票数量: " + str(short(d)[0]) + "\n")
-        f.write("融资余额: " + str(short(d)[1]) + "\n")
+
+        try:
+            f.write("融券、总市值、上市股票数量: " + str(short(d)[0]) + "\n")
+        except TypeError:
+            f.write("融券、总市值、上市股票数量: " + "no data" + "\n")
+        try:
+            f.write("融资余额: " + str(short(d)[1]) + "\n")
+        except TypeError:
+            f.write("融资余额: " + "no data" + "\n")
+
         f.write("\n")
