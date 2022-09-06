751c1083dcbc76e73958be148d26c902682eb946
rechain22 <guorch29@gmail.com>
2022-09-06 13:25:20 +0800

    证券行业2020-2022.8地量排行

--- my_scripts/zhengquan_volumn.py
+++ my_scripts/zhengquan_volumn.py
@@ -1,10 +1,17 @@
 import akshare as ak
 
-hangye = ak.stock_board_industry_name_ths()
-zhengquan = ak.stock_board_industry_index_ths(
-    symbol="证券", start_date="20200101", end_date="20220902"
-)
-a = zhengquan.sort_values(by=["成交额"]).iloc[:20, :]
-a["成交额"] = a["成交额"].div(1e8)
-b = a[["日期", "成交额"]]
-print(b)
+
+def to_csv():
+    zhengquan = ak.stock_board_industry_index_ths(
+        symbol="证券", start_date="20200101", end_date="20220902"
+    )
+    a = zhengquan.sort_values(by=["成交额"]).iloc[:20, :]
+    a["成交额"] = a["成交额"].div(1e8)
+    b = a[["日期", "成交额"]]
+    print(b)
+    b.to_csv("zhengquan_volumn_2022.csv")
+
+
+if __name__ == "__main__":
+    # to_csv()
+    pass
