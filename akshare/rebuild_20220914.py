28b37f7e5cfe09e817c391740698d0e34475814e
Moz's PC <guorch29@gmail.com>
2022-09-14 20:53:15 +0800

    Revert "Revert "恢复一下北上资金的数据（excel文件丢了)""
    This reverts commit d9ad90ffcf03eb1a9bcf16e5cc8e0ff0bd3d1840.

---
+++ akshare/rebuild_20220914.py
@@ -0,0 +1,24 @@
+import pathlib
+from unicodedata import category
+
+p = pathlib.Path("D:\\我的坚果云\\data_xlsx 数据记录\\20220617 Daily data")
+
+all_file = list(p.glob("*.txt"))
+print("------------")
+# print(all_file[0].name)
+
+n = 0
+for file in all_file:
+    if "instant" in file.name:
+        n += 1
+        print(n, " ", "filename:", file.name)
+        date = file.name[:8]
+        # category = file.name[9:]
+        with file.open(mode="r", encoding="utf-8") as f:
+            lines = f.readlines()
+        for line in lines:
+            print(line)
+            if "北上资金" in line:
+                with open("beixiang.txt", "a", encoding="utf-8") as fi:
+                    fi.write(str(date) + " " + line + "\n")
+
