459890fba0221cbd19766a7769b524f091fe1192
Moz's PC <guorch29@gmail.com>
2022-09-15 19:37:12 +0800

    处理融券/总市值 short文件中的数据

--- akshare/rebuild_20220914.py
+++ akshare/rebuild_20220914.py
@@ -1,15 +1,23 @@
 import pathlib
-from unicodedata import category
+
+
+def init():
+    p = pathlib.Path("D:\\我的坚果云\\data_xlsx 数据记录\\20220617 Daily data")
+    all_files = list(p.glob("*.txt"))
+    print("------------")
+    return all_files
 
 
 def instant_files():
     n = 0
-    for file in all_file:
+    all_files = init()
+    for file in all_files:
         if "instant" in file.name:
             n += 1
             print(n, " ", "filename:", file.name)  # 文件个数+文件名
             date = file.name[:8]
             # category = file.name[9:]
+
             with file.open(mode="r", encoding="utf-8") as f:
                 lines = f.readlines()
             for line in lines:
@@ -19,11 +27,34 @@ def instant_files():
                         fi.write(str(date) + " " + line + "\n")
 
 
+def short_files():
+    n = 0
+    all_files = init()
+    for file in all_files:
+        if "short" in file.name:
+            n += 1
+            if n < 18:  # 从7-11开始
+                continue
+            print(n - 17, " ", "filename:", file.name)  # 文件个数+文件名
+            date = file.name[:8]
+
+            with file.open(mode="r", encoding="utf-8") as f:
+                lines = f.readlines()
+
+            result = []
+            for line in lines:
+                print(line)
+                if "融券" in line:
+                    if "no data" not in line:
+                        result.append(line)
+                        with open("short_recover_20220915.txt", "a", encoding="utf-8") as fi:
+                            if result:
+                                fi.write(str(date) + " " + result[-1] + "\n")
+                                continue
+                            else:
+                                fi.write(str(date) + " " + "\n")
+
+
 if __name__ == "__main__":
-    p = pathlib.Path("D:\\我的坚果云\\data_xlsx 数据记录\\20220617 Daily data")
-
-    all_file = list(p.glob("*.txt"))
-    print("------------")
-    # print(all_file[0].name)
-
-    instant_files()
+    # instant_files()
+    short_files()
