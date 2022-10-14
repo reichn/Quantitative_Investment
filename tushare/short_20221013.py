7acbe4597df065805ddeb9596869f4f1fa2f55d5
Moz's PC <guorch29@gmail.com>
2022-10-14 19:34:21 +0800

    如果没有今天的数据就往前找（最多向前10天）

--- tushare/short_20221013.py
+++ tushare/short_20221013.py
@@ -100,16 +100,17 @@ def north(day=''):
         day = today
     df = pro.query('moneyflow_hsgt', trade_date=day)
     if df.empty:
-        return int(day), 'no north data'
+        return int(day), 'no data'
     return int(df['trade_date']), float(df['north_money']) / 100
 
 
 def data():
+    # today's data
     t = time.strftime("%H:%M")
     today = dt.date.today().strftime("%Y%m%d")
     today1 = dt.date.today().strftime("%Y-%m-%d %a")
 
-    north_ = north(today)[1:]
+    north_ = north(today)[1]
     sh_num, sz_num, sh_value, sz_value = market_value(today)[1:]
     sh_margin, sz_margin = margin(today)[1:]
     sh_rz, sz_rz = rong_zi(today)[1:]
@@ -128,6 +129,51 @@ def data():
         f.write("\n")
 
 
+def data_2():
+    t = time.strftime("%H:%M")
+    one_day = dt.timedelta(days=1)
+    today = dt.date.today().strftime("%Y%m%d")
+    yestoday = (dt.date.today() - one_day).strftime("%Y%m%d")
+    today1 = dt.date.today().strftime("%Y-%m-%d %a")
+
+    flag1 = 10
+    north_ = north(today)
+    d = today
+    while flag1 > 0 and 'no data' in north_:
+        north_ = north((dt.date.today() - one_day).strftime('%Y%m%d'))
+        flag1 -= 1
+
+    flag2 = 10
+    mv = market_value(today)
+    while flag2 > 0 and 'no data' in market_value(today):
+        mv = market_value((dt.date.today() - one_day).strftime('%Y%m%d'))
+        flag2 -= 1
+
+    flag3 = 10
+    rq = margin(today)
+    while flag3 > 0 and 'no data' in margin(today):
+        rq = margin((dt.date.today() - one_day).strftime('%Y%m%d'))
+        flag3 -= 1
+
+    flag4 = 10
+    rz = rong_zi(today)
+    while flag4 > 0 and 'no data' in rong_zi(today):
+        rz = rong_zi((dt.date.today() - one_day).strftime('%Y%m%d'))
+        flag4 -= 1
+
+    path = 'D:\\我的坚果云\\data_xlsx 数据记录\\20220617 Daily data'
+    with open(path + "\\" + today + "_data.txt", "a", encoding="utf-8") as f:
+        f.write(today1 + " " + t + "\n")  # date + week + time
+        f.write("北上资金： " + " ".join(map(str, north_)) + "\n")
+        # f.write("历史新高： " + str(len(list(his_high()))))
+        # f.write("一年新低： " + str(len(list(year_low()))))
+        f.write("成交比： " + trade_ratio() + "\n")
+        f.write("股票数量、总市值： " + " ".join(map(str, mv)) + "\n")
+        f.write("融券余额、：" + " ".join(map(str, rq)) + "\n")
+        f.write("融资余额： " + " ".join(map(str, rz)) + "\n")
+        f.write("\n")
+
+
 if __name__ == "__main__":
     # print(stocks_num())
     # print(margin('20221013'))
@@ -135,4 +181,4 @@ if __name__ == "__main__":
     # print(rong_zi('20221013'))
     # print(north('20221013'))
     # print(trade_ratio('20221013'))
-    data()
+    data_2()
