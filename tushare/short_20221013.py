dfd386d0b047eb04ae24a343190a39bf3280f609
Moz's PC <guorch29@gmail.com>
2022-10-15 11:44:04 +0800

    合并 margin 和 rong_zi；各种输出数据的格式等

--- tushare/short_20221013.py
+++ tushare/short_20221013.py
@@ -39,14 +39,16 @@ def margin(day=''):
         day = today
     margin_ = pro.query('margin', trade_date=day, exchange_id='')
     if margin_.empty:
-        return int(day), 'no_data', 'no_data'
+        return int(day), 'no_data', 'no_data', 'no_data', 'no_data'
     elif 'SSE' in margin_.values and 'SZSE' not in margin_.values:
-        return int(day), 'has_data', 'no_data'
+        return int(day), 'has_data', 'no_data', 'has_data', 'no_data'
     elif 'SSE' not in margin_.values and 'SZSE' in margin_.values:
-        return int(day), 'no_data', 'has_data'
+        return int(day), 'no_data', 'has_data', 'no_data', 'has_data'
 
-    result = margin_[['trade_date', 'exchange_id', 'rqye']]
-    return int(result.iloc[0][0]), result.iloc[0][2], result.iloc[1][2] / 1e8
+    result = margin_[['trade_date', 'exchange_id', 'rqye', 'rzye']]
+    return int(result.iloc[0][0]), result.iloc[0][2], result.iloc[1][2] / 1e8, \
+           result.iloc[0][2] / 1e8, result.iloc[1][
+               2] / 1e8
 
 
 @earlier_data
@@ -55,44 +57,24 @@ def market_value(day=''):
     today = dt.date.today().strftime("%Y%m%d")
     if not day:
         day = today
-    df1 = pro.daily_info(trade_date=day,
-                         fields="trade_date,ts_name,ts_code, com_count,total_mv")
-    # print(df1)
-    if df1.empty:
+    df = pro.daily_info(trade_date=day,
+                        fields="trade_date,ts_name,ts_code, com_count,total_mv")
+    # print(df)
+    if df.empty:
         return int(day), 'no_data', 'no_data', 'no_data', 'no_data'
-    sh_a = df1.loc[df1['ts_name'] == '上海A股']
-    sh_b = df1.loc[df1['ts_name'] == '上海B股']
-    sh_kc = df1.loc[df1['ts_name'] == '科创板']
+    sh_a = df.loc[df['ts_name'] == '上海A股']
+    sh_b = df.loc[df['ts_name'] == '上海B股']
+    sh_kc = df.loc[df['ts_name'] == '科创板']
     sh_num = int(sh_a['com_count']) + int(sh_b['com_count']) + int(
         sh_kc['com_count'])
     sh_value = float(sh_a['total_mv']) + float(sh_b['total_mv']) + float(
         sh_kc['total_mv'])
 
-    sz = df1.loc[df1['ts_name'] == '深圳市场']
+    sz = df.loc[df['ts_name'] == '深圳市场']
     sz_num = int(sz['com_count'])
     sz_value = float(sz['total_mv'])
 
-    return int(day), sh_num, sz_num, sh_value, sz_value
-
-
-@earlier_data
-def rong_zi(day=''):
-    # 融资余额
-    today = dt.date.today().strftime("%Y%m%d")
-    if not day:
-        day = today
-
-    margin_ = pro.query('margin', trade_date=day, exchange_id='')
-    if margin_.empty:
-        return int(day), 'no_data', 'no_data'
-    elif 'SSE' in margin_.values and 'SZSE' not in margin_.values:
-        return int(day), 'has_data', 'no_data'
-    elif 'SSE' not in margin_.values and 'SZSE' in margin_.values:
-        return int(day), 'no_data', 'has_data'
-
-    result = margin_[['trade_date', 'exchange_id', 'rzye']]
-    return int(result.iloc[0][0]), result.iloc[0][2] / 1e8, result.iloc[1][
-        2] / 1e8
+    return int(day), sh_num, sz_num, sh_value, sz_value, int(day)
 
 
 @earlier_data
@@ -117,8 +99,8 @@ def trade_ratio(day=''):
     if df.empty:
         return 'no_data'
     df_sort = df.sort_values(by="pct_chg", ascending=False).dropna()
-    N = df_sort.shape[0]
-    half = floor(N / 2)
+    n = df_sort.shape[0]
+    half = floor(n / 2)
     first_sum = df_sort["amount"][:half].sum()
     second_sum = df_sort["amount"][half + 1:].sum()
     ratio = (first_sum - second_sum) / df_sort["amount"].sum()
@@ -146,7 +128,7 @@ def data():
     north_ = north(today)[1]
     sh_num, sz_num, sh_value, sz_value = market_value(today)[1:]
     sh_margin, sz_margin = margin(today)[1:]
-    sh_rz, sz_rz = rong_zi(today)[1:]
+    # sh_rz, sz_rz = rong_zi(today)[1:]
 
     path = 'D:\\我的坚果云\\data_xlsx 数据记录\\20220617 Daily data'
     with open(path + "\\" + today + "_data.txt", "a", encoding="utf-8") as f:
@@ -155,26 +137,26 @@ def data():
         # f.write("历史新高： " + str(len(list(his_high()))))
         # f.write("一年新低： " + str(len(list(year_low()))))
         f.write("成交比： " + trade_ratio() + "\n")
-        f.write("股票数量： " + str(sh_num) + " " + str(sz_num) + "\n")
+        f.write("股票数量： " + str(sh_num) + "" + str(sz_num) + "\n")
         f.write("融券余额、总市值：" + str(sh_margin) + " " + str(
             sz_margin) + " " + str(sh_value) + " " + str(sz_value) + "\n")
-        f.write("融资余额： " + str(sh_rz) + " " + str(sz_rz) + "\n")
+        # f.write("融资余额： " + str(sh_rz) + " " + str(sz_rz) + "\n")
         f.write("\n")
 
 
 def data_2():
     t = time.strftime("%H:%M")
-    one_day = dt.timedelta(days=1)
+    # one_day = dt.timedelta(days=1)
     today = dt.date.today().strftime("%Y%m%d")
     # yesterday = (dt.date.today() - one_day).strftime("%Y%m%d")
     today1 = dt.date.today().strftime("%Y-%m-%d %a")
 
     north_ = north(today)
     mv = market_value(today)
-    rq = margin(today)
-    rz = rong_zi(today)
+    rq = margin(today)[:3]
+    rz = margin(today)[3:]
 
-    file_name = str(max(list(zip(north_, mv, rq, rz))[0]))  # last date
+    file_name = str(max(list(zip(north_, mv, rq))[0]))  # last date
     path = 'D:\\我的坚果云\\data_xlsx 数据记录\\20220617 Daily data'
     with open(path + "\\" + file_name + "_data.txt", "a",
               encoding="utf-8") as f:
@@ -183,9 +165,14 @@ def data_2():
         # f.write("历史新高： " + str(len(list(his_high()))))
         # f.write("一年新低： " + str(len(list(year_low()))))
         f.write("成交比： " + " ".join(map(str, trade_ratio())) + "\n")  # 这个不回溯
-        f.write("股票数量、总市值： " + " ".join(map(str, mv)) + "\n")
-        f.write("融券余额：" + " ".join(map(str, rq)) + "\n")
+        f.write(
+            "股票数量、融券、总市值： " + " ".join(
+                map(str, mv[:3])) + " | " + " ".join(
+                map(str, list(rq) + list(mv[3:]))) + "\n")
         f.write("融资余额： " + " ".join(map(str, rz)) + "\n")
+
+    with open(path + "\\" + file_name + "_data.txt", "a",
+              encoding="utf-8") as f:
         f.write("\n")
 
 
