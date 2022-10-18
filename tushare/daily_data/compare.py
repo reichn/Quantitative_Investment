4cd72afb381a5e728667cd2fb81b44a1ef0d0196
Moz's PC <guorch29@gmail.com>
2022-10-18 19:30:34 +0800

    比较10-18的成交比，数据来源为tushare和choice。算出来的结果一致。看来是excel的算法有问题，也可能是没有数据清洗和去除异常值。其实两个数据来源的总数是不太一致的，但结果有一点点误差，几乎相近。

---
+++ tushare/daily_data/compare.py
@@ -0,0 +1,24 @@
+from math import floor
+
+import pandas as pd
+
+df1 = pd.read_csv('20221018.csv')
+df2 = pd.read_csv('choice1018.csv', na_values='——')
+
+df1.reset_index()
+
+
+def ratio(df_sort):
+    df_sort.dropna()
+    n = df_sort.shape[0]
+    half = floor(n / 2)
+    first_sum = df_sort["amount"].loc[:half].sum()
+    second_sum = df_sort["amount"].loc[half + 1:].sum()
+    out = (first_sum - second_sum) / df_sort["amount"].sum()
+    return out
+
+
+a = ratio(df1)
+b = ratio(df2)
+print("{:.2%}, {:.2%}".format(float(a), float(b)))
+_ = input()
